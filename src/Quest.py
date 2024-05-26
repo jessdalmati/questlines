bf3a9f5463726977de611d622511cb5cd05a010d
Jess Dalmati <jessica.dalmati@students.mq.edu.au>
2024-05-26 13:37:26 +1000

    i remember how i feel about python

--- src/Quest.py
+++
@@ -1,7 +0,0 @@
-class Quest: 
-    def __init__(self, desc, index):
-        self.desc = desc
-        self.index = index
-
-    def __str__(self) -> str:
-        return str(self.index) + "\t" + self.desc
